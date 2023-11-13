cdb48a396354908b637d5e88b444b82ce9efd3ae
Martin <martinbruun1995@gmail.com>
2023-11-13 23:02:10 +0100

    Player buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-	import Changethemebtn from '../components/changethemebtn.svelte';
+	import { writable } from 'svelte/store'
+	import { goto } from '$app/navigation'
+	import Changethemebtn from '$lib/components/changethemebtn.svelte'
+
+	export const player = writable('')
+
+	function playAs(symbol: string) {
+		player.set(symbol)
+		goto('/game')
+	}
 </script>
 
 <main class="flex flex-col items-center justify-center h-screen gap-5">
 	<Changethemebtn />
 	<h1 class="text-8xl">Tic-tac-toe</h1>
-	<div>
-		<button class="btn btn-primary">Play</button>
+	<div class="w-[300px] flex justify-evenly">
+		<button on:click={() => playAs('O')} class="btn btn-primary">Play as O</button>
+		<button on:click={() => playAs('X')} class="btn btn-primary">Play as X</button>
 	</div>
 </main>
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Changethemebtn from '$lib/components/changethemebtn.svelte'
+</script>
+
+<main class="flex w-screen h-screen justify-center items-center">
+	<Changethemebtn />
+	<section class="board grid grid-cols-3 h-[750px] w-[750px]">
+		<div data-cell="0" class="cell h-[250px] w-[250px] border" />
+		<div data-cell="1" class="cell h-[250px] w-[250px] border border-r-0 border-l-0 border-b-0" />
+		<div data-cell="2" class="cell h-[250px] w-[250px] border" />
+		<div data-cell="3" class="cell h-[250px] w-[250px] border border-t-0 border-b-0" />
+		<div data-cell="4" class="cell h-[250px] w-[250px] border border-r-0 border-l-0 border-b-0" />
+		<div data-cell="5" class="cell h-[250px] w-[250px] border border-t-0 border-b-0" />
+		<div data-cell="6" class="cell h-[250px] w-[250px] border" />
+		<div data-cell="7" class="cell h-[250px] w-[250px] border border-r-0 border-l-0" />
+		<div data-cell="8" class="cell h-[250px] w-[250px] border" />
+	</section>
+</main>
